41fcbd8ad1584b3e65c42d7241e8118aff5842ec
Lukáš <kyselalukas@seznam.cz>
2026-09-19 22:35:11 +0200

    Release PiTV 1.4.11 appearance navigation fix

--- pitv/pitv.py
+++ pitv/pitv.py
@@ -21,7 +21,7 @@ from store_backend import (download_direct_apk, download_github_apk,
 from update_backend import is_newer, remote_pitv_version
 
 APP_NAME = "PiTV"
-VERSION = "1.4.10"
+VERSION = "1.4.11"
 
 SYSTEM_CONFIG = Path("/etc/pitv/config.json")
 USER_CONFIG = Path.home() / ".config/pitv/config.json"
